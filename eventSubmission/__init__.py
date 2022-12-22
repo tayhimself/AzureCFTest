9e6796b0b461f664d5ab729e221687f4ac52e9a5
Salim Qadri <sqadri@rics.bwh.harvard.edu>
2022-12-22 15:57:16 -0500

    Azure functions tutorial - Python

---
+++ eventSubmission/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, msg:func.Out[func.QueueMessage]) -> str:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        msg.set(name)
+        return func.HttpResponse(f"Hello, {name}.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=400
+        )
