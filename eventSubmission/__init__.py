9f9ce2a2271cb4232b1d4a1daad0d7d0abf95f86
Salim Qadri <sqadri@rics.bwh.harvard.edu>
2022-12-28 17:49:23 -0500

    Storage tables used with output bindings

--- eventSubmission/__init__.py
+++ eventSubmission/__init__.py
@@ -4,31 +4,30 @@ import json
 import azure.functions as func
 
 
-def main(req: func.HttpRequest, msg:func.Out[func.QueueMessage], message: func.Out[str]) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def main(
+    req: func.HttpRequest, msg: func.Out[func.QueueMessage], message: func.Out[str]
+) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
+    name = req.params.get("name")
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get("name")
 
     if name:
         msg.set(name)
         rowKey = str(uuid.uuid4())
-        data = {
-            "Name": "Output binding message",
-            "PartitionKey": "message",
-            "RowKey": rowKey
-        }
+        data = {"Name": "message", "PartitionKey": "mytesttable", "RowKey": rowKey}
+
         message.set(json.dumps(data))
 
         return func.HttpResponse(f"Hello, {name}.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=400
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=400,
         )
